76508ec2571f45acdf90331f60fcecb4c9b287b1
qbao <dangquocbao9504@gmail.com>
2025-02-17 19:52:19 +0700

    web runs smooth

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -9,16 +9,27 @@ function App() {
     const [sourceCode, setSourceCode] = useState("");
     const [stdout, setStdout] = useState("This is stdout content.");
     const [stderr, setStderr] = useState("This is stderr content.");
+    const [executionInfo, setExecutionInfo] = useState(null);
+    const [isLoading, setIsLoading] = useState(false);
 
     const handleRunCode = async () => {
         setStdout("");
         setStderr("");
+        setExecutionInfo(null);
+        setIsLoading(true);
+
         const judge = new Judge();
         await judge.execute(sourceCode, language);
         const result = judge.result();
-        console.log(result);
+
         setStdout(result.stdout);
         setStderr(result.stderr);
+        setIsLoading(false);
+        setExecutionInfo({
+            status: "Run success",
+            time: `${result.time}ms`,
+            memory: `${result.memory}KB`,
+        });
     };
     return (
         <div className="h-screen w-full flex flex-col gap-4 bg-primary text-white overflow-hidden">
@@ -27,6 +38,7 @@ function App() {
                 setStdout={setStdout}
                 setStderr={setStderr}
                 onRunCode={handleRunCode}
+                isLoading={isLoading}
             />
             <div className="flex gap-1">
                 <CodeEditor
@@ -36,6 +48,13 @@ function App() {
                 />
                 <Output stdout={stdout} stderr={stderr} />
             </div>
+
+            {executionInfo && (
+                <div className="fixed bottom-2 right-4 z-100 text-white p-3 rounded-lg shadow-lg font-semibold">
+                    {executionInfo.status}, {executionInfo.time},{" "}
+                    {executionInfo.memory}
+                </div>
+            )}
         </div>
     );
 }
